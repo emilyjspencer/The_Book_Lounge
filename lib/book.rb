6ccada6778326a7855e7edbff7544041a5abffe5
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-14 23:37:40 +0000

    Creates Book model and Book model spec. Tests passing

---
+++ lib/book.rb
@@ -0,0 +1,34 @@
+require_relative 'database_connection' 
+
+class Book
+  attr_reader :title, :author, :genre, :isbn, :id
+
+  def initialize(title:, author:, genre:, isbn:, id:)
+    @title = title
+    @author = author
+    @genre = genre
+    @isbn = isbn
+    @id = id
+  end 
+
+  
+  def self.create_entry(title:, author:, genre:, isbn: )
+    result = DatabaseConnection.query("INSERT INTO the_book_lounge (title, author, genre, isbn) VALUES('#{title}', '#{author}', '#{genre}', '#{isbn}') RETURNING id, title, author, genre, isbn;")
+    Book.new(id: result[0]['id'], 
+    title: result[0]['title'], 
+    author: result[0]['author'], 
+    genre: result[0]['genre'], 
+    isbn: result[0]['isbn'])
+  end 
+
+  def self.all 
+    result = DatabaseConnection.query("SELECT * FROM the_book_lounge;") 
+    result.map do |book|
+      Book.new(id: book['id'], title: book['title'], author: book['author'], genre: book['genre'], isbn: book['isbn'])
+    end
+  end
+
+  
+
+
+end 
---
+++ spec/book_spec.rb
@@ -0,0 +1,30 @@
+require 'book'
+require 'database_helpers'
+
+ describe Book do
+ 
+   describe '.create_entry' do
+     it 'creates a new book entry' do
+     book = Book.create_entry(title: 'Frankenstein', author: 'Mary Shelley', genre: 'gothic', isbn: '9781977841438')
+     persisted_data = persisted_data(table: 'the_book_lounge', id: book.id)
+     expect(book).to be_a Book
+     expect(book.id).to eq persisted_data.first['id'] 
+     expect(book.author).to eq 'Mary Shelley' 
+   end
+  end 
+
+  describe '.all' do
+    it 'shows all of the books' do
+      connection = PG.connect(dbname: 'the_book_lounge_test')  # connect to the test database
+      book = Book.create_entry(title: 'Frankenstein', author: 'Mary Shelley', genre: 'gothic', isbn: '9781977841438')
+      Book.create_entry(title: 'Business for Beginners', author: 'Lara Bryan', genre: 'non-fiction', isbn: '9781474940139') # second book for test purposes 
+      books = Book.all 
+      expect(book).to be_a Book
+      expect(book.author).to eq "Mary Shelley"
+      expect(book.title).to eq "Frankenstein"
+      expect(book.genre).to eq "gothic" 
+      expect(book.isbn).to eq "9781977841438"
+    end
+  end
+
+end 
